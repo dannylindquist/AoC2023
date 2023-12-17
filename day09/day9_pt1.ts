c13a4638913078281d0f2661ed15a7f93cea5de8
dannylindquist <danny@fromdl.com>
2023-12-17 14:56:03 -0800

    day9

---
+++ day09/day9_pt1.ts
@@ -0,0 +1,35 @@
+function calculateDifference(digits: number[]): number {
+  const diffs: number[][] = [digits];
+  let currentDigits = digits;
+  while (!diffs.at(-1)?.every((entry) => entry === 0)) {
+    const currentDiffs = [];
+    for (let i = 1; i < currentDigits.length; i++) {
+      currentDiffs.push(currentDigits[i] - currentDigits[i - 1]);
+    }
+    currentDigits = currentDiffs;
+    diffs.push(currentDiffs);
+  }
+  diffs.reverse();
+  let number = 0;
+  for (const items of diffs) {
+    number = number + items.at(-1)!;
+  }
+  return number;
+}
+
+function processData(data: string) {
+  const lines = data.split("\n");
+  let diffTotal = 0;
+  for (const line of lines) {
+    diffTotal += calculateDifference(line.split(" ").map(Number));
+  }
+  return diffTotal;
+}
+
+console.log(
+  processData(`0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`)
+);
+
+console.log(processData(await Bun.file("./day09/text.txt").text()));
